b0a16ff264e4e3203523ec3ed6951d414805e4ac
Leon.Barron <leon@tigera.io>
2024-10-31 14:16:49 +0000

    Cleaned policies

--- archive/clean-test-pols.sh
+++ archive/clean-test-pols.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-file="charts/test-policies/templates/03-policies.yaml"
+file="/Users/lbarron/Documents/argocd-final/argocd-charts/charts/test-policies/templates/03-policies.yaml"
 
 # Replace crd.projectcalico.org/v1 with projectcalico.org/v3
 sed -i '' 's/crd\.projectcalico\.org\/v1/projectcalico.org\/v3/g' "$file"
 
 # Remove quotes around numbers
-sed -i '' -E 's/"([0-9]+)"/\1/g' "$file"
+#sed -i '' -E 's/"([0-9]+)"/\1/g' "$file"
+sed -i '' -E "s/'([0-9]+)'/\1/g" "$file"
 
 # Remove specific entries
 sed -i '' '/serviceAccountSelector: ""/d' "$file"
